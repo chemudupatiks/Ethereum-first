2da63019b15e5a874c0164467b8858e071b21257
chemudupatiks <39997602+chemudupatiks@users.noreply.github.com>
2018-06-20 22:39:50 +0530

    Update Homework3.sol

--- Homework3.sol
+++ Homework3.sol
@@ -1,19 +1,29 @@
 pragma solidity ^0.4.20;
 
+mapping (string => address) quoteRegistry;
+
 contract myContract{
-	struct Quoter{
-		address addressOfQuoter;
-		string [] quotes;
+
+  function () {
+    msg.sender.send(msg.value);
+  }
+
+	function registerQuote (string _quote) {
+    quoteRegistry[_quote] = msg.sender;
 	}
-	mapping (address => Quoter) storage quoteRegistry;
-	function registerQuote (string _quote) public {
-		quoteRegistry[Quoter({
-			
-	}
+
 	function ownership (string _quote) returns (address) {
+    return quoteRegistry[_quote];
 	}
+
 	function transfer(string _quote, address _newOwner) payable {
+    if(msg.value >= 500000000000000000){
+      quoteRegistry[_quote]= _newOwner;
+    }else if(msg.value > 0){
+      msg.sender.send(msg.value);
+    }
 	}
 	function owner () returns (address){
+    return address(this);
 	}
 }
